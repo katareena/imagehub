474285a429c53ffbf922c60b08624d7c5a6e54e1
katareena <w10160177@gmail.com>
2023-09-15 15:56:00 +0300

    adds lazy loading to optimize performance

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import { AppProvider } from '../../hooks/use-context';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AppRoute, InfoText } from '../../constants/constants';
-import Layout from '../../pages/layout/layout';
-import Favourites from '../../pages/favourites/favourites';
-import Results from '../../pages/results/results';
-import Popular from '../../pages/popular/popular';
-import InfoMessage from '../info-message/info-message';
+const Layout = lazy(() => import('../../pages/layout/layout'));
+const Popular = lazy(() => import('../../pages/popular/popular'));
+const InfoMessage = lazy(() => import('../info-message/info-message'));
+const Favourites = lazy(() => import('../../pages/favourites/favourites'));
+const Results = lazy(() => import('../../pages/results/results'));
 
 function App() {
   return (
@@ -17,12 +17,12 @@ function App() {
             <Route index element={<Popular />} />
             <Route path={AppRoute.Favourites} element={<Favourites />} />
             <Route path={AppRoute.Search} element={<Results />} />
-            <Route path={AppRoute.NoFoundPage} element={
-              <InfoMessage 
-                text={InfoText.NotExistPage}
-                goToRoot={true}
-              />
-              } />
+            <Route
+              path={AppRoute.NoFoundPage}
+              element={
+                <InfoMessage text={InfoText.NotExistPage} goToRoot={true} />
+              }
+            />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import './layout.scss';
+import { InfoText } from '../../constants/constants';
 import Header from '../../components/header/header';
+import InfoMessage from '../../components/info-message/info-message';
 
 const Layout = (): JSX.Element => {
   return (
     <div className='layout'>
       <Header />
-      <Outlet />
+
+      <Suspense fallback={<InfoMessage text={InfoText.Loading}/>}>
+        <Outlet />
+      </Suspense> 
+      
     </div>
   );
 };
